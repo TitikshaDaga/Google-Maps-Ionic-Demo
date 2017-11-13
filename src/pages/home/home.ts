996eb71a49fe5d2bf905a3763ebce3fa03fa59ac
TitikshaDaga <titikshadaga19@gmail.com>
2017-11-13 16:19:29 +0530

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,71 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component,ViewChild, ElementRef} from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
+ 
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  @ViewChild('map') mapElement:ElementRef;
+  map: GoogleMap;
+  constructor(public navCtrl: NavController,public platform:Platform) {
+    platform.ready().then(()=>{
+      this.loadMap();
+    })
   }
+
+ // ionViewDidLoad() {
+   // this.loadMap();
+   //}
+
+   loadMap() {
+    
+        let mapOptions: GoogleMapOptions = {
+          camera: {
+            target: {
+              lat: 43.0741904,
+              lng: -89.3809802
+            },
+            zoom: 18,
+            tilt: 30
+          }
+        };
+    
+        this.map = GoogleMaps.create('map_canvas', mapOptions);
+    
+        // Wait the MAP_READY before using any methods.
+        this.map.one(GoogleMapsEvent.MAP_READY)
+          .then(() => {
+            console.log('Map is ready!');
+    
+            // Now you can use all methods safely.
+            this.map.addMarker({
+                title: 'Ionic',
+                icon: 'blue',
+                animation: 'DROP',
+                position: {
+                  lat: 43.0741904,
+                  lng: -89.3809802
+                }
+              })
+              .then(marker => {
+                marker.on(GoogleMapsEvent.MARKER_CLICK)
+                  .subscribe(() => {
+                    alert('clicked');
+                  });
+              });
+    
+          });
+      }
 
 }
